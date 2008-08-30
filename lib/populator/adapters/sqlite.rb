f49e7d6cac20d8643b229ba61ac84379203dca44
Ryan Bates <ryan@railscasts.com>
2008-08-30 14:36:26 -0700

    wrap sqlite inserts in transaction to improve performance

--- lib/populator/adapters/sqlite.rb
+++ lib/populator/adapters/sqlite.rb
@@ -3,7 +3,11 @@ module Populator
     module Sqlite
       # Executes multiple SQL statements in one query when joined with ";"
       def execute_batch(sql, name = nil)
-        catch_schema_changes { log(sql, name) { @connection.execute_batch(sql) } }
+        catch_schema_changes do
+          log(sql, name) do
+            @connection.transaction { |db| db.execute_batch(sql) }
+          end
+        end
       end
       
       def populate(table, columns, rows, name = nil)
